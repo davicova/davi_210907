28aa0d838551c1fe1ece3886f42244084bbe5154
davicova <86981409+davicova@users.noreply.github.com>
2021-09-08 14:21:26 -0300

    Add files via upload

---
+++ Davi_210907.py
@@ -0,0 +1,11 @@
+print("Question 1 ************************")
+for item in range(50,61):
+    print(item)
+print("Question 2 ************************")
+for item in range(50,61):
+        if item > 56 > 52:
+            print(item)
+print("Question 3 ************************")
+for item in range(55,60):
+        if item < 55 > 50:
+            print(item)
